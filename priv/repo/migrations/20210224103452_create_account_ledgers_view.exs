42ba6259058ea273fa161ffee09c953a5e15fb64
Otto Laitila <otto@laiti.la>
2021-02-24 20:40:06 +0200

    Handle account types on account_ledgeres view

--- priv/repo/migrations/20210224103452_create_account_ledgers_view.exs
+++ priv/repo/migrations/20210224103452_create_account_ledgers_view.exs
@@ -12,17 +12,34 @@ defmodule Bookkeeping.Repo.Migrations.CreateAccountLedgersView do
           SELECT
                   entries.credit_id,
                   entries.id,
-                  (0 - entries.amount)
+                  CASE WHEN accounts.type = 'asset' then
+                    (0 - entries.amount)
+                  WHEN accounts.type = 'expense' then
+                    (0 - entries.amount)
+                  ELSE
+                    entries.amount
+                  END
           FROM
                   entries
+          INNER JOIN accounts
+          ON accounts.id = entries.credit_id
+
           UNION ALL
+
           SELECT
                   entries.debit_id,
                   entries.id,
-                  entries.amount
+                  CASE WHEN accounts.type = 'asset' then
+                    entries.amount
+                  WHEN accounts.type = 'expense' then
+                    entries.amount
+                  ELSE
+                    (0 - entries.amount)
+                  END
           FROM
-                  entries;
-
+                  entries
+          INNER JOIN accounts
+          ON accounts.id = entries.debit_id;
       """,
       ""
     )
